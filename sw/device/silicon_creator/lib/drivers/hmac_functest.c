lowRISC/opentitan

[drivers] Add hmac/kmac tests for unaligned inputs
Add tests that ensure the hmac and kmac drivers properly handle
unaligned input messages.

Signed-off-by: Ellis Sarza-Nguyen <sarzanguyen@google.com>

sw/device/silicon_creator/lib/drivers/hmac_functest.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <stdalign.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -19,16 +20,17 @@
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
-enum {
-  kSha256BlockBits = 512,
-  kSha256BlockBytes = kSha256BlockBits / 8,
-};
-
 // From: http://www.abrahamlincolnonline.org/lincoln/speeches/gettysburg.htm
-static const char kGettysburgPrelude[] =
+static alignas(uint32_t) const char kGettysburgPrelude[] =
     "Four score and seven years ago our fathers brought forth on this "
     "continent, a new nation, conceived in Liberty, and dedicated to the "
     "proposition that all men are created equal.";
+
+enum {
+  kSha256BlockBits = 512,
+  kSha256BlockBytes = kSha256BlockBits / 8,
+  kGettysburgPreludeSize = sizeof(kGettysburgPrelude),
+};
 
 // The following shell command will produce the sha256sum and convert the
 // digest into valid C hexadecimal constants:
@@ -88,9 +90,39 @@ static const uint32_t kGettysburgHmacSha256Digest[] = {
     0xb54e0af5, 0x9d85e15b, 0xa9a3bafe, 0x9d115197,
 };
 
+// The unaligned version of the kGettysburgPrelude is used to test
+typedef struct unalignedhash {
+  char unused;
+  const char payload[kGettysburgPreludeSize];
+} unalignedhash_t;
+
+// This structure is used to ensure that the kGettysburgPrelude is not aligned
+// on a 4-byte boundary
+static const alignas(uint32_t) unalignedhash_t kGettysburgPreludeUnaligned = {
+    .payload =
+        "Four score and seven years ago our fathers brought forth on this "
+        "continent, a new nation, conceived in Liberty, and dedicated to the "
+        "proposition that all men are created equal.",
+};
+
 rom_error_t hmac_hmac_sha256_test(void) {
   hmac_digest_t digest;
   hmac_hmac_sha256(kGettysburgPrelude, sizeof(kGettysburgPrelude) - 1, kHmacKey,
+                   /*big_endian_digest=*/false, &digest);
+  const size_t len = ARRAYSIZE(digest.digest);
+  for (int i = 0; i < len; ++i) {
+    LOG_INFO("word %d = 0x%08x", i, digest.digest[i]);
+    if (digest.digest[i] != kGettysburgHmacSha256Digest[i]) {
+      return kErrorUnknown;
+    }
+  }
+  return kErrorOk;
+}
+
+rom_error_t hmac_hmac_sha256_unaligned_test(void) {
+  hmac_digest_t digest;
+  hmac_hmac_sha256(kGettysburgPreludeUnaligned.payload,
+                   sizeof(kGettysburgPreludeUnaligned.payload) - 1, kHmacKey,
                    /*big_endian_digest=*/false, &digest);
   const size_t len = ARRAYSIZE(digest.digest);
   for (int i = 0; i < len; ++i) {
@@ -291,6 +323,7 @@ OTTF_DEFINE_TEST_CONFIG();
 bool test_main(void) {
   status_t result = OK_STATUS();
   EXECUTE_TEST(result, hmac_sha256_test);
+  EXECUTE_TEST(result, hmac_hmac_sha256_unaligned_test);
   EXECUTE_TEST(result, hmac_hmac_sha256_test);
   EXECUTE_TEST(result, hmac_process_nowait_test);
   EXECUTE_TEST(result, hmac_process_wait_test);

LouisTheLuis/pavona

(cherry picked from commit 7e07c8b769aa71d7781637feccd0dee99baa3fd8)

sw/device/silicon_creator/lib/drivers/hmac_functest.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <stdalign.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -19,16 +20,17 @@
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
-enum {
-  kSha256BlockBits = 512,
-  kSha256BlockBytes = kSha256BlockBits / 8,
-};
-
 // From: http://www.abrahamlincolnonline.org/lincoln/speeches/gettysburg.htm
-static const char kGettysburgPrelude[] =
+static alignas(uint32_t) const char kGettysburgPrelude[] =
     "Four score and seven years ago our fathers brought forth on this "
     "continent, a new nation, conceived in Liberty, and dedicated to the "
     "proposition that all men are created equal.";
+
+enum {
+  kSha256BlockBits = 512,
+  kSha256BlockBytes = kSha256BlockBits / 8,
+  kGettysburgPreludeSize = sizeof(kGettysburgPrelude),
+};
 
 // The following shell command will produce the sha256sum and convert the
 // digest into valid C hexadecimal constants:
@@ -88,10 +90,40 @@ static const uint32_t kGettysburgHmacSha256Digest[] = {
     0xb54e0af5, 0x9d85e15b, 0xa9a3bafe, 0x9d115197,
 };
 
+// The unaligned version of the kGettysburgPrelude is used to test
+typedef struct unalignedhash {
+  char unused;
+  const char payload[kGettysburgPreludeSize];
+} unalignedhash_t;
+
+// This structure is used to ensure that the kGettysburgPrelude is not aligned
+// on a 4-byte boundary
+static const alignas(uint32_t) unalignedhash_t kGettysburgPreludeUnaligned = {
+    .payload =
+        "Four score and seven years ago our fathers brought forth on this "
+        "continent, a new nation, conceived in Liberty, and dedicated to the "
+        "proposition that all men are created equal.",
+};
+
 rom_error_t hmac_hmac_sha256_test(void) {
   hmac_digest_t digest;
   sc_hmac_hmac_sha256(kGettysburgPrelude, sizeof(kGettysburgPrelude) - 1,
                       kHmacKey,
+                      /*big_endian_digest=*/false, &digest);
+  const size_t len = ARRAYSIZE(digest.digest);
+  for (int i = 0; i < len; ++i) {
+    LOG_INFO("word %d = 0x%08x", i, digest.digest[i]);
+    if (digest.digest[i] != kGettysburgHmacSha256Digest[i]) {
+      return kErrorUnknown;
+    }
+  }
+  return kErrorOk;
+}
+
+rom_error_t hmac_hmac_sha256_unaligned_test(void) {
+  hmac_digest_t digest;
+  sc_hmac_hmac_sha256(kGettysburgPreludeUnaligned.payload,
+                      sizeof(kGettysburgPreludeUnaligned.payload) - 1, kHmacKey,
                       /*big_endian_digest=*/false, &digest);
   const size_t len = ARRAYSIZE(digest.digest);
   for (int i = 0; i < len; ++i) {
@@ -292,6 +324,7 @@ OTTF_DEFINE_TEST_CONFIG();
 bool test_main(void) {
   status_t result = OK_STATUS();
   EXECUTE_TEST(result, hmac_sha256_test);
+  EXECUTE_TEST(result, hmac_hmac_sha256_unaligned_test);
   EXECUTE_TEST(result, hmac_hmac_sha256_test);
   EXECUTE_TEST(result, hmac_process_nowait_test);
   EXECUTE_TEST(result, hmac_process_wait_test);