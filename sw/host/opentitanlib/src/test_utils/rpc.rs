lowRISC/opentitan

[opentitanlib,ujson] add function to pad UJSON strings before TX
This adds a function to opentitanlib to enable padding a UJSON string
with whitespace before the final "}" to enable testing always constant
sized host-->device UJSON transmissions. These will be used during ATE
provisioning flows.

Signed-off-by: Tim Trippel <ttrippel@google.com>

sw/host/opentitanlib/src/test_utils/rpc.rs
@@ -22,6 +22,7 @@ where
 {
     fn send(&self, device: &T) -> Result<String>;
     fn send_with_crc(&self, device: &T) -> Result<String>;
+    fn send_with_padding(&self, device: &T, max_size: usize) -> Result<String>;
 }
 
 impl<T, U> ConsoleSend<T> for U
@@ -45,6 +46,15 @@ where
         };
         let crc_s = actual_crc.send(device)?;
         Ok(s + &crc_s)
+    }
+
+    fn send_with_padding(&self, device: &T, max_size: usize) -> Result<String> {
+        let mut s = serde_json::to_string(self)?;
+        let pad_len = max_size - s.len();
+        let pad_str = ' '.to_string().repeat(pad_len);
+        s.insert_str(s.len() - 1, &pad_str);
+        device.console_write(s.as_bytes())?;
+        Ok(s)
     }
 }
 

LouisTheLuis/pavona

(cherry picked from commit 16b435faf8e6048d2fb5c016807e77249be28ded)

sw/host/opentitanlib/src/test_utils/rpc.rs
@@ -21,6 +21,7 @@ where
 {
     fn send(&self, device: &T) -> Result<String>;
     fn send_with_crc(&self, device: &T) -> Result<String>;
+    fn send_with_padding(&self, device: &T, max_size: usize) -> Result<String>;
 }
 
 impl<T, U> ConsoleSend<T> for U
@@ -44,6 +45,15 @@ where
         };
         let crc_s = actual_crc.send(device)?;
         Ok(s + &crc_s)
+    }
+
+    fn send_with_padding(&self, device: &T, max_size: usize) -> Result<String> {
+        let mut s = serde_json::to_string(self)?;
+        let pad_len = max_size - s.len();
+        let pad_str = ' '.to_string().repeat(pad_len);
+        s.insert_str(s.len() - 1, &pad_str);
+        device.write(s.as_bytes())?;
+        Ok(s)
     }
 }
 