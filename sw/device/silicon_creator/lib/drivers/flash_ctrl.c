lowRISC/opentitan

[rom_ext] Implement Integrator Specific FW Binding (ISFB) logic.
Implements Integrator Specific FW Binding (ISFB) functionality to provide
anti-rollback and product binding functionality to application firmware.

The following changes are included:

1. Add flash_ctrl helper function to generate the info flash page
   configuration based on the ownership ISFB config.
2. Implement isfb logic as a separate module to simplify ROM_EXT
   integration.
3. Add unittest cases to verify anti-rollback and product association
   functionality.
4. Add minimum level of hardening against FI.

The ROM_EXT integration and e2e test cases will be implemented in a
follow up commit.

Signed-off-by: Miguel Osorio <miguelosorio@google.com>
(cherry picked from commit e5bd02c2af8a26ee69836720227004dab230f69d)
Signed-off-by: Miguel Osorio <miguelosorio@google.com>

sw/device/silicon_creator/lib/drivers/flash_ctrl.c
@@ -687,3 +687,34 @@ void flash_ctrl_cert_info_page_owner_restrict(
   flash_ctrl_info_perms_set(info_page, kCertificateInfoPageOwnerAccess);
   sec_mmio_write32(info_page->cfg_wen_addr, 0);
 }
+
+rom_error_t flash_ctrl_info_type0_params_build(
+    uint8_t bank, uint8_t page, flash_ctrl_info_page_t *info_page) {
+  HARDENED_CHECK_LT(bank, FLASH_CTRL_PARAM_REG_NUM_BANKS);
+  HARDENED_CHECK_LT(page, FLASH_CTRL_PARAM_NUM_INFOS0);
+
+  uint32_t cfg_addr = kBase;
+  uint32_t cfg_wen_addr = kBase;
+
+  switch (bank) {
+    case 0:
+      cfg_addr += FLASH_CTRL_BANK0_INFO0_PAGE_CFG_0_REG_OFFSET;
+      cfg_wen_addr += FLASH_CTRL_BANK0_INFO0_REGWEN_0_REG_OFFSET;
+      break;
+    case 1:
+      cfg_addr += FLASH_CTRL_BANK1_INFO0_PAGE_CFG_0_REG_OFFSET;
+      cfg_wen_addr += FLASH_CTRL_BANK1_INFO0_REGWEN_0_REG_OFFSET;
+      break;
+    default:
+      HARDENED_TRAP();
+  }
+
+  *info_page = (flash_ctrl_info_page_t){
+      .base_addr = bank * FLASH_CTRL_PARAM_BYTES_PER_BANK +
+                   page * FLASH_CTRL_PARAM_BYTES_PER_PAGE,
+      .cfg_addr = cfg_addr + page * sizeof(uint32_t),
+      .cfg_wen_addr = cfg_wen_addr + page * sizeof(uint32_t),
+  };
+
+  return kErrorOk;
+}

LouisTheLuis/pavona

(cherry picked from commit c2584252c0851f0940d6889b5833f6ae606e9675)

sw/device/silicon_creator/lib/drivers/flash_ctrl.c
@@ -720,3 +720,34 @@ void flash_ctrl_cert_info_page_owner_restrict(
   flash_ctrl_info_perms_set(info_page, kCertificateInfoPageOwnerAccess);
   sec_mmio_write32(flash_ctrl_core_base() + info_page->cfg_wen_offset, 0);
 }
+
+rom_error_t flash_ctrl_info_type0_params_build(
+    uint8_t bank, uint8_t page, flash_ctrl_info_page_t *info_page) {
+  HARDENED_CHECK_LT(bank, FLASH_CTRL_PARAM_REG_NUM_BANKS);
+  HARDENED_CHECK_LT(page, FLASH_CTRL_PARAM_NUM_INFOS0);
+
+  uint32_t cfg_offset = 0;
+  uint32_t cfg_wen_offset = 0;
+
+  switch (bank) {
+    case 0:
+      cfg_offset = FLASH_CTRL_BANK0_INFO0_PAGE_CFG_0_REG_OFFSET;
+      cfg_wen_offset += FLASH_CTRL_BANK0_INFO0_REGWEN_0_REG_OFFSET;
+      break;
+    case 1:
+      cfg_offset = FLASH_CTRL_BANK1_INFO0_PAGE_CFG_0_REG_OFFSET;
+      cfg_wen_offset += FLASH_CTRL_BANK1_INFO0_REGWEN_0_REG_OFFSET;
+      break;
+    default:
+      HARDENED_TRAP();
+  }
+
+  *info_page = (flash_ctrl_info_page_t){
+      .base_addr = bank * FLASH_CTRL_PARAM_BYTES_PER_BANK +
+                   page * FLASH_CTRL_PARAM_BYTES_PER_PAGE,
+      .cfg_offset = cfg_offset + page * sizeof(uint32_t),
+      .cfg_wen_offset = cfg_wen_offset + page * sizeof(uint32_t),
+  };
+
+  return kErrorOk;
+}